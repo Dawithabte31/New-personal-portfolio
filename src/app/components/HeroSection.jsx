d30df83a0698884ed834bc4eaf80090fb0d690ce
Dawithabte31 <dawithabitamu31@gmail.com>
2024-07-11 11:07:35 +0300

    "Updated HeroSection component, removed hero-image.png, and changed Tailwind config colors"

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -17,7 +17,7 @@ const HeroSection = () => {
         >
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">
-              Hello, I&apos;m{" "}
+              Hello, I&apos;m Dawit{" "}
             </span>
             <br></br>
             <TypeAnimation
@@ -47,14 +47,16 @@ const HeroSection = () => {
             >
               Hire Me
             </Link>
-            <Link
-              href="/"
+
+            <a
+              href="/Dawit Habitamu Tewonde (2).pdf"
+              download="Dawit Habitamu Tewonde (2).pdf"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
                 Download CV
               </span>
-            </Link>
+            </a>
           </div>
         </motion.div>
         <motion.div
@@ -63,11 +65,12 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative overflow-hidden">
             <Image
-              src="/images/hero-image.png"
+              // src="/images/hero-image.png"
+              src="/images/dawit.png"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
+              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 object-contain md:w-[500px] md:w-[400px]"
               width={300}
               height={300}
             />
